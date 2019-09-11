99762816b1f3f4e6bf07f2667d73ba97d23502a4
Anoob Bava <anoob.bava@gmail.com>
2019-09-11 12:14:56 +0530

    Added Design for NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <h1>NavBar Content</h1>
+    <v-toolbar dark>
+        <v-toolbar-title
+          @click="redirectToHome">My todos
+        </v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+      <v-toolbar-items>
+        <v-btn text>Add Todo</v-btn>
+        <v-btn text>Register</v-btn>
+        <v-btn text @click="redirectToLogin">Login</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
   </div>
 </template>
 
-<style>
-</style>
+<script>
+export default {
+  methods: {
+    redirectToHome () {
+      this.$router.push('/')
+    },
+
+    redirectToLogin () {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
