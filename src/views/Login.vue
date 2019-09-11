c7d3fef47e04bedd60346f958ca3b9b35fe595cc
Anoob Bava <anoob.bava@gmail.com>
2019-09-11 12:33:56 +0530

    Added login page design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>
-    <h2>welcome to login page</h2>
-  </div>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <div class="flex-grow-1"></div>
+                <v-tooltip bottom>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
 </template>
+
+<script>
+  export default {
+  }
+</script>
