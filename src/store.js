8f6c26861515e57799f06d9ebc73887db4765ed9
Anoob Bava <anoob.bava@gmail.com>
2019-09-12 12:30:38 +0530

    added basic structure for the store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: {},
+    token: localStorage.getItem('token') || '',
+    status: null
   },
+
   mutations: {
+    // will set the status to loading
+    loadingMutation(state) { },
 
+    // will set the loading and token to success
+    loadingSucessMutation(state, token, user) { },
+
+    // this will set the loading as False
+    loadingFailureMutation(state) { }
   },
-  actions: {
 
+  actions: {
+    // this will be called from the components and call th loginMutation.
+    loginAction({ commit }, userDetails) { })
   }
 })
