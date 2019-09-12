80fa529634d3bbd80dcdff9a0f1953d6f92d4588
Anoob Bava <anoob.bava@gmail.com>
2019-09-12 12:02:38 +0530

    Added axios and fetch token from localStorage

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+// setup axios for the http requests
+Vue.prototype.$http = Axios
+
+// fetch data from localstorage
+const token = localStorage.getItem('token')
+
+// will prepend all the requests with token from localStorage
+if (token) {
+  vuetify.prototype.$http.defaults.headers.common = token
+}
 
 new Vue({
   router,
