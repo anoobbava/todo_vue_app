f4e533f7bb5bf17e9d5d00fc7372352d13a81f41
Anoob Bava <anoob.bava@gmail.com>
2019-10-15 10:40:03 +0530

    link the module to the Vuex core

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import user from '@/store/modules/user'
+import todos from '@/store/modules/todos'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    todos
   },
 
   state: {},
